37711c099f0b39b1f50696d270b541a5d1ddbdec
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-01-21 11:37:34 -0500

    my first program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,12 @@
 
+/* 
+@author: Cannon Bennett
+@Date: 1/21/2026
+@purpose: My first python program
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
