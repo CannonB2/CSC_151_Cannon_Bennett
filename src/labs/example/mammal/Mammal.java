aaec947c4c81667f41fcd051c61d5981df54da8d
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-01-28 12:43:31 -0500

    created mammal.java

---
+++ src/labs/example/mammal/Mammal.java
@@ -0,0 +1,12 @@
+/* 
+@author: Cannon Bennett
+@Date: 1/28/2026
+@purpose: Creates a Mammal class with appropriate properties and methods
+*/
+
+package labs.example.mammal;
+
+public class Mammal {
+
+    
+}
