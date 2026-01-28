8919aaeccf9656e0fdb7326a191eea392b070e49
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-01-28 11:15:33 -0500

    example directory work

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+/* 
+@author: Cannon Bennett
+@Date: 1/21/2026
+@purpose: My first python program
+*/
+
+package labs.example.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
