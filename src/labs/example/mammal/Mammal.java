9443583f313669fee5e2f093ddb225acfabfe3f1
Cannon Bennett <bennettcm503@mail.sandhills.edu>
2026-02-01 23:44:54 -0500

    finished mammal.java

--- src/labs/example/mammal/Mammal.java
+++ src/labs/example/mammal/Mammal.java
@@ -7,6 +7,46 @@
 package labs.example.mammal;
 
 public class Mammal {
+    String hairColor;
+    String eyeColor;
+    int bodyTemp;
+    String breed;
+    String height;
+    String weight;
 
-    
+    public static void run() {
+
+    }
+
+    public static void eat() {
+
+    }
+
+    public static void sleep() {
+
+    }
+
+    public static void scratch() {
+
+    }
+
+    public static void play() {
+
+    }
+
+    public static void drink() {
+
+    }
+
+    public static void walk() {
+
+    }
+
+    public static void fetch() {
+
+    }
+
+    public static void sit() {
+        
+    }
 }
