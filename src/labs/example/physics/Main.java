24d024bbfe547c4082912e844b054e87acc27f82
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-02-11 11:22:42 -0500

    completed week 5 lab

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -2,23 +2,40 @@
 @author: Cannon Bennett
 @Date: 2/9/2026
 @purpose: Main file used to call methods from Physics file
+
+Comments for compiling and running so I don't forget/can just copy and paste it
+Javac -d bin src/labs/example/physics/*.java
+Java -cp bin labs/example/physics/Main
 */
 
-// Javac -d bin src/labs/example/physics/*.java
-// Java -cp bin labs/example/physics/Main
-
+// I'm not explaining this again. Tt's in the last one, go look there.
 package labs.example.physics;
 
+// Creates Main class for use in running physics operations from the Physics class
 public class Main {
     public static void main(String[] args) {
+
+        // Creates a new instance of Physics to use
         Physics physics = new Physics();
 
-        System.out.println(physics.getDistance(5, 6) + " miles.");
+        /* 
+        Uses the different get methods from Physics for calculations and then prints the returned result to console with the proper units
+        values inside parenthesis are passed to the method for use in calculations 
+        (If the calculations don't make sense in a realistic setting that's because I used a random number generator, plus I am not a math nerd so I don't know what the results should look like)
+        */
 
-        System.out.println(physics.getVelocity(14, 2) + " mph.");
+        System.out.println(physics.getDistance(12.57, 4.19) + " miles");
 
-        System.out.println(physics.getMomentum(10, 3) + " kg m/s.");
+        System.out.println(physics.getVelocity(4.75, 15.84) + " mph");
 
-        System.out.println(physics.getForce(8, 4) + " kg m/s^2");
+        System.out.println(physics.getMomentum(10.98, 3.79) + " kg m/s");
+
+        System.out.println(physics.getForce(13.9, 14.12) + " kg m/s^2");
+
+        System.out.println(physics.getWork(12.98, 10.98) + " Joules");
+
+        System.out.println(physics.getKineticEnergy(14.72, 6.62) + " Joule");
+
+        System.out.println(physics.getPotentialEnergy(9.18, 5.52) + " Joule");
     }
 }
--- src/labs/example/physics/Physics.java
+++ src/labs/example/physics/Physics.java
@@ -4,38 +4,60 @@
 @purpose: file used to hold methods for use in physics calculations
 */
 
+// Directory path so this thing actually compiles
 package labs.example.physics;
+// Math library for math purposes
 import java.lang.Math;
 
+// Creates physics class with physic operations (a.k.a. I don't know what to comment so I'm putting random stuff here)
 public class Physics {
 
-    int x;
-    int v;
-    int p;
-    int w;
-    int f;
-    int ke;
-    int pe;
-
+    // g r a v i t y (Constant because gravity typically doesn't change)
     final static double GRAVITY = 9.81;
 
-    public double getDistance(int v, int t) {
+
+    // Creates methods for each physics operation. Each method takes an imputed value from Main, calculates it, and then returns the result
+    // The variables that are created inside each method were originally created outside of them alongside GRAVITY, but I felt like it looked cleaner this way. It might be as smart though I'm really not sure.
+    public double getDistance(double v, double t) {
+        double x;
         x = v * t;
         return x;
     }
 
-    public double getVelocity(int x, int t) {
+    public double getVelocity(double x, double t) {
+        double v;
         v = x / t;
         return v;
     }
 
-    public double getMomentum(int m, int v) {
+    public double getMomentum(double m, double v) {
+        double p;
         p = m * v;
         return p;
     }
 
-    public double getForce(int m, int a) {
+    public double getForce(double m, double a) {
+        double f;
         f = m * a;
         return f;
+    }
+
+    public double getWork(double f, double d) {
+        double w;
+        w = f * d;
+        return w;
+    }
+
+    public double getKineticEnergy(double m, double v) {
+        double ke;
+        // Used pow from math instead of multiplying v by itself to square it because I'm cool (I'm not)
+        ke = 0.5 * m * Math.pow(v, 2);
+        return ke;
+    }
+
+    public double getPotentialEnergy(double m, double h) {
+        double pe;
+        pe = m * GRAVITY * h;
+        return pe;
     }
 }
