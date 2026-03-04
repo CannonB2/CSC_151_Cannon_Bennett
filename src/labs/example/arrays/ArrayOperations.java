9130c2489f0c1c896c622e7f5f9ea2ad27a61b17
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-03-04 12:53:27 -0500

    Help me this sorting makes no sense

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -26,10 +26,44 @@ public class ArrayOperations {
             System.out.println(values[count]);
         }
     }
-
+    
     public void sortArray(int[] numbers) {
         int[] numbersSorted = new int[50];
+        int lastHighest = 200;
+        int lastLowest = 0;
 
-        
+        for (int count = 0; count <= numbers.length-1; count++) {
+            int highNum = 0;
+            int lowNum = 200;
+
+            for (int index = 0; index <= numbers.length-1; index++) {
+
+                if ((numbers[index] > highNum) && (highNum < lastHighest)) {
+                    highNum = numbers[index];
+                    System.out.println("high: " + highNum);
+                }
+                if ((numbers[index] < lowNum) && (lowNum > lastLowest)) {
+                    lowNum = numbers[index];
+                    System.out.println("low: " + lowNum);
+                }
+            }
+            
+            numbersSorted[count] = lowNum;
+            numbersSorted[numbersSorted.length-count-1] = highNum;
+
+            if (highNum < lastHighest) {
+                    lastHighest = highNum;
+                    System.out.println("highest: " + lastHighest);
+            }
+            if (lowNum > lastLowest) {
+                lastLowest = lowNum;
+                System.out.println("lowest: " + lastLowest);
+            }
+        }
+
+        for (int count = 0; count <= numbersSorted.length-1; count++) {
+            System.out.println(numbersSorted[count]);
+        }
     }
+    
 }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -16,5 +16,10 @@ public class Main {
         ArrayOperations arrayOperations = new ArrayOperations();
 
         arrayOperations.createNewArray(5);
+
+        int[] numberList = {11, 5, 92, 30, 70, 77, 63, 60, 83, 89, 95, 48, 78, 55, 28, 92, 80, 32, 53, 47,
+                            46, 10, 27, 24, 25, 88, 6, 73, 62, 36, 3, 76, 93, 71, 25, 31, 94, 38, 45, 26,
+                            113, 192, 75, 58, 138, 148, 4, 165, 161, 159};
+        arrayOperations.sortArray(numberList);
     }
 }
