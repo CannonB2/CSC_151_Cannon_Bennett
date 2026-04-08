f3202360507948a83dcf72ec896fdb6ac91350ee
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-04-08 12:22:01 -0400

    Worked further on week 13 lab

--- src/labs/example/fileOperations/Logger.java
+++ src/labs/example/fileOperations/Logger.java
@@ -10,21 +10,66 @@ Java -cp bin labs/example/fileOperations/Logger
 
 package labs.example.fileOperations;
 
+import java.io.*;
+import java.util.*;
+
 public class Logger {
 
     private static final String BASE_FILE_PATH = "T:\\CSC_151_Cannon_Bennett\\src\\labs\\example\\fileOperations";
     private static final String API_LOGS_FILE = BASE_FILE_PATH + "\\logs\\api_error.log";
 
     public static void main(String[] args) {
-        Logger logger = new Logger();
-        BufferedReader log;
-        log = logger.openErrorLog();
+        try {
+            Logger logger = new Logger();
+            BufferedReader log;
+            log = logger.openErrorLog();
+            logger.getCountOfErrorTypes(log);
+            log.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error! could not get Buffer Reader object.");
+        }
     }
 
     private BufferedReader openErrorLog() {
-        File apiLogFile = new File(API_LOGS_FILE);
+        BufferedReader br = null;
 
-        BufferedReader br = new BufferedReader(new FileReader(API_LOGS_FILE));
+        try {
+            File apiLogFile = new File(API_LOGS_FILE);
+            br = new BufferedReader(new FileReader(API_LOGS_FILE));
+        }
+        catch (IOException e) {
+            System.out.println("Error! could not get Buffer Reader object.");
+        }
         return br;
+    }
+
+    private void getCountOfErrorTypes(BufferedReader file) {
+        try {
+            int warn = 0;
+            int error = 0;
+            int info = 0;
+            int debug = 0;
+            String line;
+
+            while ((line = file.readLine()) != null) {
+                String[] value = line.split("\\[|\\]");
+                if (value[1].equals("WARN")) {
+                    warn++;
+                } else if (value[1].equals("ERROR")) {
+                    error++;
+                } else if (value[1].equals("INFO")) {
+                    info++;
+                } else if (value[1].equals("DEBUG")) {
+                    debug++;
+                }
+            }
+
+            System.out.println("These are the number of different error types:");
+            System.out.println("Warn: " + warn + "\n Error: " + error + "\n Info: " + info + "\n Debug: " + debug);
+        }
+        catch (IOException e) {
+            System.out.println("Error! could not get Buffer Reader object.");
+        }
     }
 }
