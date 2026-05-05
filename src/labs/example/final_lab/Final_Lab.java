b89be466e4bb2b32de0e4242736bae777a81bf5b
Cannon Bennett <bennettcm503@mail.sandhills.edu>
2026-05-05 16:24:10 -0400

    Started working on final lab

---
+++ src/labs/example/final_lab/Final_Lab.java
@@ -0,0 +1,26 @@
+/* 
+@author: Cannon Bennett
+@Date: 4/20/2026
+@purpose: 
+
+Comments for compiling and running so I don't forget/can just copy and paste it
+Javac -d bin src/labs/example/final_lab/*.java
+Java -cp bin labs/example/final_lab/Final_Lab
+*/
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+package labs.example.final_lab;
+
+public class Final_Lab {
+    
+}
