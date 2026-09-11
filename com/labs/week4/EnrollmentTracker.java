df51dedecdd0afb8595e06e66f9bf9bc30c21dea
Cannon Bennett <bennettcm503@mail.sandhills.edu>
2026-09-11 01:37:47 -0400

    Finished week 4 lab

--- com/labs/week4/EnrollmentTracker.java
+++ com/labs/week4/EnrollmentTracker.java
@@ -1,5 +1,72 @@
 package com.labs.week4;
+import java.util.*;
 
 public class EnrollmentTracker {
-    
+
+    // creates a method to check if a students name is stored in the ArrayList
+    public static boolean studentExists(ArrayList<String> list, String name) {
+
+        // creates a new variable that stores whether or not a name exists
+        boolean exists = false;
+
+        // runs a loop over the list. If the name is found then sets the exists variable to true
+        for (String entry : list) {
+            if (entry == name) {
+                exists = true;
+            }
+        }
+
+        // returns the exists variable
+        return exists;
+    }
+
+    // creates a method that removes a student from the ArrayList
+    public static void removeStudent(ArrayList<String> list, String name) {
+
+        // checks if the students name exists and assigns the result to a variable
+        boolean exists = studentExists(list, name);
+
+        // if the student does not exist prints a message and removes them and prints a message otherwise
+        if (!exists) {
+            System.out.println("Name was not found. Please enter a valid name.");
+        } else {
+            // runs a loop over the list till coming across the stored name and then removes it
+            for (int i = 0; i <= list.size()-1; i++) {
+                if (list.get(i) == name) {
+                    list.remove(i);
+                    System.out.println("Student successfully removed.");
+                }
+            }
+        }
+    }
+
+    // creates a method to add a student as long as it does not already exist
+    public static void addStudent(ArrayList<String> list, String name) {
+
+        // checks if the students name exists and assigns the result to a variable
+        boolean exists = studentExists(list, name);
+
+        // if the name does not exist then adds it to the list and prints a message
+        if (!exists) {
+            list.add(name);
+            System.out.println("Student successfully added.");
+        }
+    }
+
+    // creates a method that checks how many times a name exists in the list
+    public static int countOccurences(ArrayList<String> list, String name) {
+
+        // creates a variable to store the amount of times the name occurs
+        int occurences = 0;
+
+        // runs a for-loop over the list. if the entry is the same as the name then increases occurences.
+        for (String entry : list) {
+            if (entry == name) {
+                occurences++;
+            }
+        }
+
+        // returns the amount of times the name has occured
+        return occurences;
+    }
 }
--- com/labs/week4/Main.java
+++ com/labs/week4/Main.java
@@ -8,6 +8,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // creates new enrollment tracker instance
+        EnrollmentTracker enrollmentTracker = new EnrollmentTracker();
+        
+        // creates new ArrayList containing student names stored as strings and adds names to it
         ArrayList<String> students = new ArrayList<>();
         students.add("Lily");
         students.add("Trixie");
@@ -18,12 +22,42 @@ public class Main {
         students.add("Damion");
         students.add("Opal");
 
+        // Runs two different for-loops over students and print the names stored inside
+        System.out.println("--------------------------------------");
         for (int i = 0; i < students.size(); i++) {
             System.out.println(students.get(i));
         }
-
+        System.out.println("--------------------------------------");
         for (String name : students) {
             System.out.println(name);
         }
+
+        // checks if a name is stored in the students array and prints a message
+        System.out.println("--------------------------------------");
+        System.out.println("Student exists: " + enrollmentTracker.studentExists(students, "Opal"));
+        System.out.println("--------------------------------------");
+
+        // runs methods to remove three students and add a student
+        enrollmentTracker.removeStudent(students, "Sam");
+        enrollmentTracker.removeStudent(students, "Lily");
+        enrollmentTracker.removeStudent(students, "Opal");
+        enrollmentTracker.addStudent(students, "Artemis");
+
+        // prints the names in students to show everything was proper removed and added
+        System.out.println("--------------------------------------");
+        for (String name : students) {
+            System.out.println(name);
+        }
+        System.out.println("--------------------------------------");
+
+        // adds two duplicate students and then runs methods to show how many times different names occur
+        students.add("Damion");
+        students.add("Damion");
+        System.out.println("Damion's name occurs " + enrollmentTracker.countOccurences(students, "Damion") + " times");
+        System.out.println("Jules' name occurs " + enrollmentTracker.countOccurences(students, "Jules") + " times");
+        System.out.println("Hank's name occurs " + enrollmentTracker.countOccurences(students, "Hank") + " times");
+        System.out.println("--------------------------------------");
+
+        // I think we may have mentioned something about part 7 but I can't remember. The instructions aren't there so either way I can't really do it.
     }
 }
